1cda91cc57fffb1d18c57820a65d18c33b47f601
Ayenem <moub.ahmed@hotmail.com>
2021-05-06 18:37:30 -0400

    1 is not a prime

--- include/eul/is_prime.hpp
+++ include/eul/is_prime.hpp
@@ -18,7 +18,7 @@ constexpr auto oddly_factorable(int const n_to_factor) -> bool
 
 constexpr auto is_prime(int const i) -> bool
 {
-  if(i == 1 or i == 2) return true;
+  if(i == 2) return true;
   if(i % 2 == 0) return false;
 
   return not oddly_factorable(i);
