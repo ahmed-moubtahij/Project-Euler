86344801f46c07fce7ed56e9f4a8e80e25163c3d
Ayenem <moub.ahmed@hotmail.com>
2021-05-09 13:38:52 -0400

    refactoring: rm <utility>

--- src/P4.cpp
+++ src/P4.cpp
@@ -1,6 +1,5 @@
 #include <eul/k_combinations.hpp>
 #include <eul/is_palindrome.hpp>
-#include <functional> // std::multiplies
 #include <range/v3/view/filter.hpp>
 #include <fmt/ranges.h>
 
@@ -9,7 +8,9 @@ int main()
   namespace rv = ranges::views;
   namespace stdr = std::ranges;
 
-  auto product_combinations = eul::k_combinations<2>(100, 1000, std::multiplies{});
+  constexpr auto bin_multiply = [](int a, int b){ return a * b; } ;
+  
+  auto product_combinations = eul::k_combinations<2>(100, 1000, bin_multiply);
   
   auto palindromes = product_combinations
                    | rv::filter(eul::is_palindrome);
