833cd4bb56de5ab8bc7332be0c14fdc069b15ac0
Ayenem <moub.ahmed@hotmail.com>
2021-05-09 18:15:50 -0400

    hoisted k_cartesian

--- include/eul/k_combinations.hpp
+++ include/eul/k_combinations.hpp
@@ -10,12 +10,16 @@
 
 namespace eul{
 
-template<std::size_t K, std::size_t ...Is>
+template<std::size_t K>
 [[nodiscard]] constexpr
-auto k_cartesian(int from, int to, std::index_sequence<Is...>) noexcept
+auto k_cartesian(int from, int to) noexcept
 {
   namespace rv = ranges::views;
-  return rv::cartesian_product((void(Is), rv::ints(from, to))...);
+  return [from, to]<std::size_t ...Is>(std::index_sequence<Is...>)
+  {
+    return rv::cartesian_product((void(Is), rv::ints(from, to))...);
+  }
+  (std::make_index_sequence<K>());
 }
 
 template<std::size_t K>
@@ -24,16 +28,10 @@ auto k_combinations(int from, int to, auto op) noexcept
 {
   namespace rv = ranges::views;
 
-  //auto const k_cartesian =
-  //[from, to]<std::size_t ...Is>(std::index_sequence<Is...>)
-  //{
-  //  return rv::cartesian_product((void(Is), rv::ints(from, to))...);
-  //};
-  
   auto const fold_tuple = [op](auto common_tuple)
   { return std::apply(op, std::move(common_tuple)); };
   
-  return k_cartesian<K>(from, to, std::make_index_sequence<K>())
+  return k_cartesian<K>(from, to)
        | rv::transform(fold_tuple);
 }
 
