d8a72f746775127e77bca3f8b6af7ccf596eeb69
Ayenem <moub.ahmed@hotmail.com>
2021-05-09 11:43:12 -0400

    P8 commit

---
+++ include/eul/conversion.hpp
@@ -0,0 +1,12 @@
+#ifndef EUL_CONVERSION_HPP
+#define EUL_CONVERSION_HPP
+
+namespace eul{
+  
+  constexpr auto ctoi(char c) -> int {
+	return c - '0';
+  }
+
+} // namespace eul
+
+#endif // EUL_CONVERSION_HPP
