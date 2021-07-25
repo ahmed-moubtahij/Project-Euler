96bf238ef3f6401ad5f65ca6c65ce64d13852dc9
Ayenem <50707385+Ayenem@users.noreply.github.com>
2021-07-25 18:25:08 -0400

    std::pair -> named struct

--- src/P12.cpp
+++ src/P12.cpp
@@ -1,8 +1,8 @@
 // NOTE: TIMES OUT ON GODBOLT, NEEDS TESTING ON VS
-// https://godbolt.org/z/Wqvr9Ex83
+// https://godbolt.org/z/5sTzsxds4
 
 #include <fmt/ranges.h>
-#include <range/v3/view/iota.hpp> // ranges::views::ints
+#include <range/v3/view/indices.hpp>
 #include <range/v3/view/transform.hpp>
 #include <range/v3/view/filter.hpp>
 #include <range/v3/view/partial_sum.hpp>
@@ -16,32 +16,31 @@ template<typename T = std::uint32_t>
 [[nodiscard]] constexpr
 auto triangle_nb_with_over_n_divisors(unsigned n) noexcept
 {
-  using pair_t = std::pair<T, T>;
-  auto pair_with_factors_count = [](T n) -> pair_t
+  struct P{ T triangle_nb; T factors_count; };
+
+  auto pair_with_factors_count = [](T n) -> P
   {
     auto factors =
-      rv::ints(T{ 1 }, static_cast<T>(n + 1))
+      rv::indices(1u, n + 1u)
       | rv::filter([n](T const i){ return n % i == 0; });
-    return {n, r::distance(std::move(factors))};
+    return {n, static_cast<T>(r::distance(std::move(factors)))};
   };
   
   auto triangles_with_factors_count =
-    rv::ints
+    rv::indices
     | rv::partial_sum // triangle numbers
     | rv::transform(std::move(pair_with_factors_count));
 
-  auto [triangle_nb, factors_count] = *r::find_if(
+  return (*r::find_if(
     triangles_with_factors_count,
     [n](T const count){ return count > n; },
-    &pair_t::second
-  );
-
-  return triangle_nb;
+    &P::factors_count
+  )).triangle_nb;
 }
 
 int main(){
-  // constexpr unsigned n_divisors{ 5 }; // works
-  constexpr unsigned n_divisors{ 500 };
+  constexpr unsigned n_divisors{ 5 }; // works
+  // constexpr unsigned n_divisors{ 500 };
   
   fmt::print("{}\n",
     triangle_nb_with_over_n_divisors(n_divisors)
