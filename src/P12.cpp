7f33e49b34033e4304b9a4fe9096c742c680332f
Ayenem <moub.ahmed@hotmail.com>
2021-07-27 15:19:34 -0400

    refactored

--- src/P12.cpp
+++ src/P12.cpp
@@ -1,45 +1,27 @@
-#include <fmt/ranges.h>
+#include <fmt/core.h>
 #include <range/v3/view/indices.hpp>
 #include <range/v3/view/transform.hpp>
 #include <range/v3/view/filter.hpp>
 #include <range/v3/view/partial_sum.hpp>
 #include <range/v3/algorithm/find_if.hpp>
-#include <cstdint> // std::uint32_t
+#include <eul/numbers_properties.hpp>
 
 namespace r = ranges;
 namespace rv = r::views;
 
-// TODO: Move this to include/eul/
-namespace eul{
-  template<std::integral T>
-  struct divides{
-    T m;
-    [[nodiscard]] constexpr
-    auto operator()(T const i) const -> bool
-    { return m % i == 0; }
-  };
-
-  template<std::integral T>
-  struct greater_than{
-    T m;
-    [[nodiscard]] constexpr
-    auto operator()(T const i) const -> bool
-    { return i > m; }
-  };
-}
-
-template<typename T = std::uint32_t>
-[[nodiscard]] constexpr
-auto triangle_nb_with_over_n_divisors(unsigned n) noexcept
+template<std::unsigned_integral T>
+[[nodiscard]]
+auto triangle_nb_with_over_n_divisors(T n) noexcept -> T
 {
   struct P{ T triangle_nb; T factors_count; };
 
-  auto append_factors_count = [](T n) -> P
+  auto append_factors_count = [](T triangle_nb) -> P
   {
-    return {n,
+    return {
+      triangle_nb,
       static_cast<T>(r::distance(
-          rv::indices(1u, n + 1u)
-        | rv::filter(eul::divides{ n })
+          rv::indices(T{ 1 }, triangle_nb + T{ 1 })
+        | rv::filter(eul::divides{ triangle_nb })
       ))
     };
   };
@@ -57,8 +39,8 @@ auto triangle_nb_with_over_n_divisors(unsigned n) noexcept
 }
 
 int main(){
-  constexpr unsigned n_divisors{ 5 }; // works
-  // constexpr unsigned n_divisors{ 500 };
+  //constexpr unsigned n_divisors{ 300 }; // works
+   constexpr unsigned n_divisors{ 500 }; // too much compile time; range-v3's fault
   
   fmt::print("{}\n",
     triangle_nb_with_over_n_divisors(n_divisors)
